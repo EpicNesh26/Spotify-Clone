8d7e4236bf4f33975384784a3cee00a0f88ced28
Nesh <soninesh2006@gmail.com>
2024-05-29 19:23:37 +0530

    Added songs

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 console.log("Lets start Javascript")
+
+
+async function main() {
+    let a = await fetch("C:\Coding\Frontend Projects\Spotify Clone\songs");
+    let response = await a.text();
+    console.log(response)
+    let div = document.createElement("div");
+    div.innerHTML = response;
+    let as = div.getElementsByTagName(".mp3")
+    console.log(as)
+
+}
+
+
