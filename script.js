34eef62871fd90dfa4b82d87b6f44f3350e1d821
Nesh <soninesh2006@gmail.com>
2024-05-29 20:41:21 +0530

    Modified

--- script.js
+++ script.js
@@ -1,15 +1,29 @@
 console.log("Lets start Javascript")
 
 
-async function main() {
-    let a = await fetch("C:\Coding\Frontend Projects\Spotify Clone\songs");
-    let response = await a.text();
-    console.log(response)
-    let div = document.createElement("div");
-    div.innerHTML = response;
-    let as = div.getElementsByTagName(".mp3")
-    console.log(as)
+// async function main() {
+//     let a = await fetch("http://127.0.0.1:5500/songs/");
+//     let response = await a.text();
+//     console.log(response)
+//     let div = document.createElement("div");
+//     div.innerHTML = response;
+//     let as = div.getElementsByTagName("a");
+//     let songs = []
+//     for (let index = 0; index < as.length; index++) {
+//         const element = array[index];
+//         if(element.href.endsWith(".mp3")){
+//             songs.push(element.href)
 
-}
+//         }
+//     }
+//     return songs
 
+// }
 
+// async function main(){
+//     let songs = await getSongs()
+//     console.log(songs)
+
+//     var audio = new Audio(songs[0]);
+//     audio.play();
+// }
